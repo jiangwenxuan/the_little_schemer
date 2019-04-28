6147bb0a9ce88ce0c5c0ae37affcc1aad46117ce
jwx <904653684@qq.com>
2019-04-28 10:32:16 +0800

    read Toys

--- toys.scm
+++ toys.scm
@@ -1,8 +1,13 @@
 #lang racket/load
-(load "perface.rkt")
+(load "perface.scm")
 
-(display (atom? 'atom))
-(display (atom? 'turkey))
-(display (atom? '1492))
-(display (list? '(atom)))
-(display (list? '(atom turkey or)))
+;(display (atom? 'atom))
+;(display (atom? 'turkey))
+;(display (atom? '1492))
+;(display (list? '(atom)))
+;(display (list? '(atom turkey or)))
+
+(define s (list 'a 'b (list 'c)))
+(define l (list 'd 'e 'f 'g))
+(define a (cons s l))
+(display a)
