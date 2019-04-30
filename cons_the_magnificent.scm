84125edcc59aeceac3959a2e62c58cef2cc4d4a7
jwx <904653684@qq.com>
2019-04-30 09:34:43 +0800

    continue cons the magnificent

--- cons_the_magnificent.scm
+++ cons_the_magnificent.scm
@@ -18,5 +18,108 @@
                           (rember a (cdr lat)))))))))
 
 
-(define lat (list 'a 'b 'c 'd 'b 'd 'c 'b))
-(display (rember 'b lat))
+;(define lat (list 'a 'b 'c 'd 'b 'd 'c 'b))
+;(display (rember 'b lat))
+
+(define new-firsts
+  (lambda (l)
+    (cond
+      ((null? l) '())
+      (else
+       (cons (car (car l)) (new-firsts (cdr l)))))))
+
+;(define l (list (list 'a 'b) (list 'd 'e) (list 'q 'b)))
+;(display (new-firsts l))
+
+(define new-insertR
+  (lambda (new old lat)
+    (cond
+      ((null? lat) '())
+      ((eq? old (car lat)) (cons old
+                                 (cons new (cdr lat))))
+      (else
+       (cons (car lat) (new-insertR new old (cdr lat)))))))
+
+(define insertR
+  (lambda (new old lat)
+    (cond
+      ((null? lat) '())
+      (else
+       (cond
+         ((eq? old (car lat))
+          (cons old (cons new (cdr lat))))
+         (else (cons (car lat)
+                     (insertR new old (cdr lat)))))))))
+
+;(define lat1 (list 'a 'c 'd 'e 's 'h))
+;(display (insertR 'z 'd lat1))
+
+(define insertL
+  (lambda (new old lat)
+    (cond
+      ((null? lat) '())
+      (else
+       (cond
+         ((eq? old (car lat)) (cons new lat))
+         (else
+          (cons (car lat) (insertL new old (cdr lat)))))))))
+
+(define subst
+  (lambda (new old lat)
+    (cond
+      ((null? lat) '())
+      (else (cond
+              ((eq? old (car lat)) (cons new (cdr lat)))
+              (else (cons (car lat)
+                          (subst (new old (cdr lat))))))))))
+
+(define subst2
+  (lambda (new o1 o2 lat)
+    (cond
+      ((null? lat) '())
+      (else (cond
+              ((eq? o1 (car lat))
+               (cons new (cdr lat)))
+              ((eq? o2 (car lat))
+               (cons new (cdr lat)))
+              (else (cons (car lat)
+                          (subst2 new o1 o2 (cdr lat)))))))))
+
+(define better-subst
+  (lambda (new o1 o2 lat)
+    (cond
+      ((null? lat) '())
+      (else (cond
+              ((or (eq? o1 (car lat))
+                   (eq? o2 (car lat)))
+               (cons new (cdr lat)))
+              (else (cons (car lat)
+                          (better-subst new o1 o2 (cdr lat)))))))))
+
+(define multirember
+  (lambda (a lat)
+    (cond
+      ((null? lat) '())
+      (else
+       (cond
+         ((eq? a (car lat)) (multirember a (cdr lat)))
+         (else (cons (car lat) (multirember a (cdr lat)))))))))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
