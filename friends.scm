bbde6c9912d8e3b64784cb1eea2ba97ea0b9137c
jwx <904653684@qq.com>
2019-05-10 22:48:35 +0800

    friends and relations

--- friends.scm
+++ friends.scm
@@ -73,18 +73,131 @@
   (lambda (set1 set2)
     (and (subset? set1 set2) (subset? set2 set1))))
 
+(define intersect?
+  (lambda (set1 set2)
+    (cond
+      ((null? set1) #f)
+      ((member? (car set1) set2) #t)
+      (else
+       (intersect? (cdr set1) set2)))))
 
+(define easy-intersect?
+  (lambda (set1 set2)
+    (cond
+      ((null? set1) #t)
+      (else
+       (or (member? (car set1) set2)
+           (easy-intersect? (cdr set1) set2))))))
 
+(define intersect
+  (lambda (set1 set2)
+    (cond
+      ((null? set1) '())
+      ((member? (car set1) set2)
+       (cons (car set1)
+             (intersect (cdr set1) set2)))
+      (else
+       (intersect (cdr set1) set2)))))
 
+(define union
+  (lambda (set1 set2)
+    (cond
+      ((null? set1) set2)
+      ((member? (car set1) set2)
+       (union (cdr set1) set2))
+      (else
+       (cons (car set1)
+             (union (cdr set1) set2))))))
 
+(define xxx
+  (lambda (set1 set2)
+    (cond
+      ((null? set1) '())
+      ((member? (car set1) set2)
+       (xxx (cdr set1) set2))
+      (else
+       (cons (car set1)
+             (xxx (cdr set1) set2))))))
 
+(define intersectall
+  (lambda (l-set)
+    (cond
+      ((null? (cdr l-set)) (car l-set))
+      (else
+       (intersect (car l-set)
+                  (intersectall (cdr l-set)))))))
 
+(define a-pair?
+  (lambda (x)
+    (cond
+      ((atom? x) #f)
+      ((null? x) #f)
+      ((null? (cdr x)) #f)
+      ((null? (car (cdr x))) #t)
+      (else #f))))
+                     
+(define first
+  (lambda (p)
+    (car p)))
 
+(define second
+  (lambda (p)
+    (car (cdr p))))
 
+(define build
+  (lambda (s1 s2)
+    (cons s1
+          (cons s2 '()))))
 
+(define third
+  (lambda (p)
+    (car (cdr (cdr p)))))
 
+(define fun?
+  (lambda (rel)
+    (set? (firsts rel))))
 
+(define firsts
+  (lambda (rel)
+    (cond
+      ((null? rel) '())
+      (else
+       (cons (first (car rel))
+             (firsts (cdr rel)))))))
 
+(define revrel
+  (lambda (rel)
+    (cond
+      ((null? rel) '())
+      (else
+       (cons (build (second (car rel))
+                    (first (car rel)))
+             (revrel (cdr rel)))))))
 
+(define revpair
+  (lambda (pair)
+    (build (second pair) (first pair))))
 
+(define easy-revrel
+  (lambda (rel)
+    (cond
+      ((null? rel) '())
+      (else
+       (cons (revpair (car rel))
+             (easy-revrel (cdr rel)))))))
 
+(define fullfun?
+  (lambda (fun)
+    (set? (seconds fun))))
+
+(define seconds
+  (lambda (rel)
+    (cond
+      ((null? rel) '())
+      (else
+       (cons (second (car rel))
+             (seconds (cdr rel)))))))
+
+(define one-to-one?
+  (lambda (fun)
+    (fun? (revrel fun))))
